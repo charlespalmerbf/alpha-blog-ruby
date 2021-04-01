9334d9e511026c1ad198400645a54b19efedfe46
charleshpalmer <73086397+charleshpalmer@users.noreply.github.com>
2021-04-01 12:40:46 +0100

    Adding Destroy Method and Form Links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -60,4 +60,14 @@ class ArticlesController < ApplicationController
 
   end
 
+  def destroy
+
+    @article = Article.find(params[:id])
+
+    @article.destroy
+
+    redirect_to articles_path
+
+  end
+
 end
